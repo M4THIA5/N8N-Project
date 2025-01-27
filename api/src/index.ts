6aeacec70c19f6d986f760543b2ec9b260c510e8
M4THIA5 <mathias.collas@gmail.com>
2025-01-27 20:23:03 +0100

    fix: minor errors

--- api/src/index.ts
+++ api/src/index.ts
@@ -7,13 +7,15 @@ import sqlite3 from 'sqlite3';
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
 export const db = new sqlite3.Database('./database/db.sqlite');
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
-app.use(bodyParser.urlencoded());
 app.use('/tasks', taskRouter);
 app.use('/lists', listRouter);
 app.use('/users', userRouter);
